510976c9cea7c271c95659331d307390fce0d20c
se-ma2024 <94701414+se-ma2024@users.noreply.github.com>
2024-01-24 12:55:47 +0900

    add Basic functions (add,delete,edit)

--- todo_list-app/models/todoModel.js
+++ todo_list-app/models/todoModel.js
@@ -3,4 +3,8 @@ const todos = [];
 module.exports = {
     getAllToDos: () => todos,
     addToDo: (todo) => todos.push(todo),
-}
+    deleteToDo: (index) => todos.splice(index, 1),
+    editToDo: (index, updatedTodo) => {
+        todos[index] = updatedTodo;
+    },
+};
