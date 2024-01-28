ba03562fae7559170b1bf022ae556b844b5e52ef
se-ma2024 <94701414+se-ma2024@users.noreply.github.com>
2024-01-29 08:04:28 +0900

    add_css

--- todo_list-app/app.js
+++ todo_list-app/app.js
@@ -8,6 +8,7 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/todos", routes);
 
